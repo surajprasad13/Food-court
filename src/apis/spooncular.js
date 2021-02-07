cacb4f9bfed6a3b51567e0292d22215d46833331
Suraj Prasad <surajvishwakarma830@gmail.com>
2021-02-08 00:57:15 +0530

    issue resolve

--- src/apis/spooncular.js
+++ src/apis/spooncular.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.spoonacular.com",
   header: {
-    "Content-type": "application/json"
-  }
+    "Content-type": "application/json",
+  },
 });
